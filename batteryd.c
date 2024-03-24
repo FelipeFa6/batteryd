7a786f0182b145e74d949a742847d33564f1649f
FelipeFa6 <felipe.farias.e1@gmail.com>
2024-03-24 18:36:08 -0300

    Indentation to 4 spaces

--- batteryd.c
+++ batteryd.c
@@ -21,149 +21,149 @@ static bool daemonize = true;
 static bool keep_running = true;
 
 static void handler(int sig) {
-  keep_running = false;
+    keep_running = false;
 }
 
 const char* config_path() {
-  /* Return a global path when running as root.  */
+    /* Return a global path when running as root.  */
 
-  if (getuid() == 1) {
-    return strdup(GLOBAL_CONFIG);
-  }
+    if (getuid() == 1) {
+        return strdup(GLOBAL_CONFIG);
+    }
 
-  /* get the calling's user directory.  */
-  char* home = getenv("HOME");
-  if (home == NULL) {
-    err(__LINE__, "%s", "HOME variable not defined");
-  }
+    /* get the calling's user directory.  */
+    char* home = getenv("HOME");
+    if (home == NULL) {
+        err(__LINE__, "%s", "HOME variable not defined");
+    }
 
-  /* build the local path.  */
-  const size_t buflen = strlen(home) + strlen(LOCAL_CONFIG) + 1;
-  char* buffer = alloca(buflen);
-  memset(buffer, 0, buflen);
-  strcat(buffer, home);
-  strcat(buffer, LOCAL_CONFIG);
-  return strdup(buffer);
+    /* build the local path.  */
+    const size_t buflen = strlen(home) + strlen(LOCAL_CONFIG) + 1;
+    char* buffer = alloca(buflen);
+    memset(buffer, 0, buflen);
+    strcat(buffer, home);
+    strcat(buffer, LOCAL_CONFIG);
+    return strdup(buffer);
 }
 
 /* state handlers.  */
 
 static void on_change(const char* cp, const char* target) {
-  size_t buflen = strlen(cp) + strlen(target) + 2;
-  char* fp = alloca(buflen);
-  dbg("state is %s", target);
+    size_t buflen = strlen(cp) + strlen(target) + 2;
+    char* fp = alloca(buflen);
+    dbg("state is %s", target);
 
-  /* create the full path.  */
-  memset(fp, 0, buflen);
-  strcat(fp, cp);
-  strcat(fp, "/");
-  strcat(fp, target);
+    /* create the full path.  */
+    memset(fp, 0, buflen);
+    strcat(fp, cp);
+    strcat(fp, "/");
+    strcat(fp, target);
 
-  /* execute the script.  */
-  switch (fork()) {
-    case -1: {
-      err(__LINE__, "fork");
+    /* execute the script.  */
+    switch (fork()) {
+        case -1: {
+                     err(__LINE__, "fork");
+                 }
+        case 0: {
+                    char* argv[] = { fp, NULL };
+                    setsid();
+                    execv(fp, argv);
+                    err(__LINE__, "%s", fp);
+                }
+        default: {
+                     int dumb;
+                     wait(&dumb);
+                     break;
+                 }
     }
-    case 0: {
-      char* argv[] = { fp, NULL };
-      setsid();
-      execv(fp, argv);
-      err(__LINE__, "%s", fp);
-    }
-    default: {
-      int dumb;
-      wait(&dumb);
-      break;
-    }
-  }
 }
 
 /*
  * State processor.
  */
 
-static void
+    static void
 process(const char* cp, struct apm_power_info* pinfo)
 {
-  static u_char state = APM_BATT_UNKNOWN;
+    static u_char state = APM_BATT_UNKNOWN;
 
-  /* check if the state has changed.  */
-  if (state == pinfo->battery_state) {
-    return;
-  }
+    /* check if the state has changed.  */
+    if (state == pinfo->battery_state) {
+        return;
+    }
 
-  state = pinfo->battery_state;
+    state = pinfo->battery_state;
 
-  /* process the new state.  */
-  switch (pinfo->battery_state) {
-    case APM_BATT_CRITICAL: {
-      on_change(cp, "critical");
-      break;
+    /* process the new state.  */
+    switch (pinfo->battery_state) {
+        case APM_BATT_CRITICAL: {
+                                    on_change(cp, "critical");
+                                    break;
+                                }
+        case APM_BATT_LOW: {
+                               on_change(cp, "low");
+                               break;
+                           }
+        case APM_BATT_HIGH: {
+                                on_change(cp, "high");
+                                break;
+                            }
+        default: {
+                     break;
+                 }
     }
-    case APM_BATT_LOW: {
-      on_change(cp, "low");
-      break;
-    }
-    case APM_BATT_HIGH: {
-      on_change(cp, "high");
-      break;
-    }
-    default: {
-      break;
-    }
-  }
 }
 
 /* arg parsing. */
 void parse_options(int argc, char** argv) {
-  int ch;
-  while ((ch = getopt(argc, argv, "d")) != -1) {
-    switch (ch) {
-      case 'd':
-        daemonize = false;
-        break;
-      default:
-        fprintf(stderr, "usage: batteryd [-d]\n");
-        exit(__LINE__);
+    int ch;
+    while ((ch = getopt(argc, argv, "d")) != -1) {
+        switch (ch) {
+            case 'd':
+                daemonize = false;
+                break;
+            default:
+                fprintf(stderr, "usage: batteryd [-d]\n");
+                exit(__LINE__);
+        }
     }
-  }
 }
 
 int main(int argc, char** argv) {
 
-  parse_options(argc, argv);
+    parse_options(argc, argv);
 
-  if (daemonize) {
-    int nochroot = getuid() != 1;
-    setsid();
-    daemon(nochroot, 0 /* noclose */);
-  }
-
-  /* grab the config path. */
-  const char* cp = config_path();
-  dbg("%s", cp);
-
-  /* open apm */
-  int fd = open("/dev/apm", O_RDONLY);
-  if (fd < 0) {
-    err(__LINE__, "open");
-  }
-
-  /* register ^c.  */
-  signal(SIGINT, handler);
-
-  /* polling loop.  */
-  while (keep_running) {
-    struct apm_power_info pinfo;
-    if (ioctl(fd, APM_IOC_GETPOWER, &pinfo) < 0) {
-      err(__LINE__, "ioctl");
+    if (daemonize) {
+        int nochroot = getuid() != 1;
+        setsid();
+        daemon(nochroot, 0 /* noclose */);
     }
-    process(cp, &pinfo);
-    sleep(1);
-  }
 
-  /* Clean-up and return. */
-  close(fd);
-  free((void*)cp);
-  return 0;
+    /* grab the config path. */
+    const char* cp = config_path();
+    dbg("%s", cp);
+
+    /* open apm */
+    int fd = open("/dev/apm", O_RDONLY);
+    if (fd < 0) {
+        err(__LINE__, "open");
+    }
+
+    /* register ^c.  */
+    signal(SIGINT, handler);
+
+    /* polling loop.  */
+    while (keep_running) {
+        struct apm_power_info pinfo;
+        if (ioctl(fd, APM_IOC_GETPOWER, &pinfo) < 0) {
+            err(__LINE__, "ioctl");
+        }
+        process(cp, &pinfo);
+        sleep(1);
+    }
+
+    /* Clean-up and return. */
+    close(fd);
+    free((void*)cp);
+    return 0;
 }
